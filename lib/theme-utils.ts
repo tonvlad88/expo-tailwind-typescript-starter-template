3af60f858c1f985765a8df2dcc3b82143537b1fa
Tony Saromines <tonysaromines@gmail.com>
2025-10-01 00:18:20 +0800

    tech: Code clean up

--- lib/theme-utils.ts
+++
@@ -1,8 +0,0 @@
-export const rgb = (value: string, alpha = 1) => {
-  return `rgba(${value}, ${alpha})`;
-};
-
-export const resolveColor = (theme: any, key: string, alpha = 1): string => {
-  const value = theme?.colors?.[key] ?? "0,0,0";
-  return rgb(value, alpha);
-};
